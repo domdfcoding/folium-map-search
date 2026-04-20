d55e5d1d786538e00dce982a00fda1a41dccc561
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-04-20 18:27:38 +0100

    Bump version v0.1.0b3 -> v0.1.0b4

--- folium_map_search/__init__.py
+++ folium_map_search/__init__.py
@@ -58,7 +58,7 @@ __all__ = [
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2026 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0b3"
+__version__: str = "0.1.0b4"
 __email__: str = "dominic@davis-foster.co.uk"
 
 # ControlPosition = "topleft" | "topright" | "bottomleft" | "bottomright"
